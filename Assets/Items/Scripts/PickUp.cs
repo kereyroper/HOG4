0521dd69cfadb008dea4cc5a3a006e6e1701f02e
Kerey Roper <kerey@rivetinggames.com>
2014-03-01 14:13:52 -0800

    item stubs

---
+++ Assets/Items/Scripts/PickUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//TODO: link to player here
+	}
+	
+	void OnTriggerEnter(Collider other) { //TODO: maybe Collider2D?
+		// TODO: Check if player
+		// Add item to player inventory if not full for this item
+	}
+}
