3b818393191b41ac8187d6449984386801cec96f
Kerey Roper <kerey@rivetinggames.com>
2014-03-01 16:53:10 -0800

    untested bagel door

---
+++ Assets/Items/Scripts/Door.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+	public Direction direction;
+
+	void OnTriggerEnter(Collider other) { //TODO: maybe Collider2D?
+		// TODO: Check if player
+		LevelCamera.MoveCamera(direction);
+	}
+}
