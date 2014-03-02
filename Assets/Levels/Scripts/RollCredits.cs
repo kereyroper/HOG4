3ed898b394c903b7e95eb2831c3ceeea196df99d
Corey Blackburn <blackburncorey@gmail.com>
2014-03-02 13:38:00 -0800

    Added credits screen.

---
+++ Assets/Levels/Scripts/RollCredits.cs
@@ -0,0 +1,61 @@
+﻿/// <summary>
+/// Roll credits.
+/// By Corey Blackburn
+/// 3/2/2014
+/// Lists the participants by section scrolling upward.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class RollCredits : MonoBehaviour
+{
+	
+	bool isRolling = false;
+	public float rollSpeed;
+	public float end;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		guiText.text =
+			section ("Artists") +
+				participant ("Rebecca Mroczkowski", "http://www.behance.net/rlmroczkowski") +
+				section ("Audio") +
+				participant ("Evan Witt", "http://www.fiverbeyond.com/") +
+				section ("Programmers") + 
+				participant ("Corey Blackburn", "http://www.coreyblackburn.com/") +
+				participant ("Maria Mckinley", "http://mariakathryn.net/") +
+				participant ("Kerey Roper", "http://www.rivetinggames.com/") +
+				participant ("Evan Witt", "http://www.fiverbeyond.com/") +
+				section ("REACTOR & HouseOgames Present: HOGjam #4");
+		
+		isRolling = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!isRolling) return;
+		
+		transform.Translate (Vector3.up * Time.deltaTime * rollSpeed);
+		
+		if (transform.position.y > end)
+		{
+			isRolling = false;
+			Debug.LogError("Need to edit the build settings.");
+			Application.LoadLevel ("doors and walls test");
+		}
+	}
+	
+	string section(string title)
+	{
+		return "\n" + title + "\n\n";
+	}
+
+	string participant(string name, string portfolio)
+	{
+		if (portfolio != "") return name + "\n" + portfolio + "\n\n";
+		else return name + "\n\n";
+	}
+}
