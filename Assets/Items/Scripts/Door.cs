5b4762d5f3a6406d19d5cdd2f0fc20725b69b8ca
Kerey Roper <kerey@rivetinggames.com>
2014-03-01 17:27:10 -0800

    adding colliders to player and bagel, bagel is a door

--- Assets/Items/Scripts/Door.cs
+++ Assets/Items/Scripts/Door.cs
@@ -5,7 +5,7 @@ public class Door : MonoBehaviour
 {
 	public Direction direction;
 
-	void OnTriggerEnter(Collider other) { //TODO: maybe Collider2D?
+	void OnTriggerEnter2D(Collider2D other) {
 		// TODO: Check if player
 		LevelCamera.MoveCamera(direction);
 	}
