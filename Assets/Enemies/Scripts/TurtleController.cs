5567cf884b47b6fd40a7fe20b98636a337e1355a
Maria McKinley <mariak@mariakathryn.net>
2014-03-01 17:12:34 -0800

    script for moving turtle

---
+++ Assets/Enemies/Scripts/TurtleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurtleController : MonoBehaviour {
+
+	public float turtleSpeed = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (-turtleSpeed, -turtleSpeed, 0);
+	}
+	void onCollisionEnter(Collision collision) {
+	}	
+}
