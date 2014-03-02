da35453f8ceccf40c1519fb1e100e4fe90adfc18
Corey Blackburn <blackburncorey@gmail.com>
2014-03-02 14:23:01 -0800

    Basic button functionality.

---
+++ Assets/Levels/Scripts/Button.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+
+	public Sprite upSprite;
+	public Sprite downSprite;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player" || col.gameObject.tag == "Turttle")
+		{
+			GetComponent<SpriteRenderer>().sprite = downSprite;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		GetComponent<SpriteRenderer>().sprite = upSprite;
+	}
+}
