947d7fe02eef76deaf17a895ddab96ff1fac38c0
Maria McKinley <mariak@mariakathryn.net>
2014-03-02 15:02:47 -0800

    main menu now works

---
+++ Assets/Levels/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI () {
+		// Make a background box
+		GUI.Box(new Rect(20,40,140,150), "Toaster Settings");
+		
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(50,70,80,20), "Start Game")) {
+			Application.LoadLevel("toaster game");
+		}
+		
+		// Make the credits button.
+		if(GUI.Button(new Rect(50,100,80,20), "Credits")) {
+			Application.LoadLevel("Credits");
+		}
+	
+		// Make the quit button.
+		if(GUI.Button(new Rect(50,130,80,20), "Quit")) {
+			Application.LoadLevel("game_over");
+		}
+
+		// Extra Dark
+		if(GUI.Button(new Rect(50,160,80,20), "Extra Dark")) {
+			Application.LoadLevel("burned_it");
+		}
+	}
+}
