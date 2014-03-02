db13ba0f976549ca77c57143933ba27c28cf8902
Corey Blackburn <blackburncorey@gmail.com>
2014-03-01 21:07:56 -0800

    Move player to new screen when entering doors.

--- Assets/Levels/Scripts/LevelCamera.cs
+++ Assets/Levels/Scripts/LevelCamera.cs
@@ -2,21 +2,30 @@
 using System.Collections;
 
 public class LevelCamera : MonoBehaviour {
-
+	
 	public static void MoveCamera(Direction direction) {
 		var cam = Camera.main;
+
+		//HACK grab player to move across the door's threshold
+		GameObject player = GameObject.Find ("Player");
+		float doorThreshold = 3.0f;
+
 		switch (direction) {
 			case Direction.Up:
 				cam.transform.position += new Vector3(0, 10.8f, 0);
+				player.transform.position += new Vector3(0, doorThreshold, 0);
 				break;
 			case Direction.Down:
 				cam.transform.position += new Vector3(0, -10.8f, 0);
+				player.transform.position += new Vector3(0, -doorThreshold, 0);
 				break;
 			case Direction.Left:
 				cam.transform.position += new Vector3(-19.2f, 0, 0);
+				player.transform.position += new Vector3(-doorThreshold, 0, 0);
 				break;
 			default:
 				cam.transform.position += new Vector3(19.2f, 0, 0);
+				player.transform.position += new Vector3(doorThreshold, 0, 0);
 				break;
 		}
 	}
