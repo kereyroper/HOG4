935b13dc1be17af7e505fa256ec2e376645f3638
Corey Blackburn <blackburncorey@gmail.com>
2014-03-02 18:52:50 -0800

    Can continue playing from game over screen.

---
+++ Assets/Levels/Scripts/GameOverScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+	
+	private float halfWidth = Screen.width / 2 - 40;
+	private float halfHeight = Screen.height / 2;
+
+	void OnGUI ()
+	{
+		if(GUI.Button(new Rect(halfWidth, halfHeight + 70,80,20), "Play Again"))
+		{
+			Application.LoadLevel("toaster game");
+		}
+		
+		// Make the credits button.
+		if(GUI.Button(new Rect(halfWidth,halfHeight + 100,80,20), "Credits"))
+		{
+			Application.LoadLevel("Credits");
+		}
+		
+		// Make the quit button.
+		if(GUI.Button(new Rect(halfWidth, halfHeight + 130,80,20), "Quit")) {
+			Application.Quit();
+		}
+	}
+}
