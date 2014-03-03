d0d11dfd03f1785ea4b73a6b17ef616ed8adc415
Corey Blackburn <blackburncorey@gmail.com>
2014-03-02 18:30:31 -0800

    Added win screen.

---
+++ Assets/Levels/Scripts/WinScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScreen : MonoBehaviour {
+
+	private float halfWidth = Screen.width / 2 - 40;
+	private float halfHeight = Screen.height / 2;
+	void OnGUI () {
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(halfWidth, halfHeight + 70,80,20), "Play Again"))
+		{
+			Application.LoadLevel("toaster game");
+		}
+		
+		// Make the credits button.
+		if(GUI.Button(new Rect(halfWidth,halfHeight + 100,80,20), "Credits"))
+		{
+			Application.LoadLevel("Credits");
+		}
+		
+		// Make the quit button.
+		if(GUI.Button(new Rect(halfWidth, halfHeight + 130,80,20), "Quit")) {
+			Application.Quit();
+		}
+	}
+}
